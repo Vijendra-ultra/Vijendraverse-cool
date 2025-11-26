ef59806000abbb9f6ed6e6f526a444299fcff6d2
Vijendra <pvijendra756@gmail.com>
2025-11-26 13:18:18 +0530

    Remove this

--- src/app/dms/page.tsx
+++ src/app/dms/page.tsx
@@ -1,24 +1,26 @@
+import Image from "next/image";
+
 const Page = () => {
   return (
     <div className="pt-24 pb-24 min-h-full">
       <div className="flex flex-col justify-center">
         <div className="mt-6 bg-green-300">
           <h2 className="text-3xl text-center">6</h2>
-          <img src="6.jpg"></img>
+          <Image src="6.jpg" alt="image" />
         </div>
         <div className="mt-6 bg-red-300">
           <h2 className="text-3xl text-center">7</h2>
           <div className="mx-auto">
-            <img className="block" src="7.jpg"></img>
+            <Image src="7.jpg" alt="image" />
           </div>
         </div>
         <div className="mt-6 bg-purple-300">
           <h2 className="text-3xl text-center">8</h2>
-          <img src="8.jpg"></img>
+          <Image src="8.jpg" alt="image" />
         </div>
         <div className="mt-6 bg-amber-400">
           <h2 className="text-3xl text-center">9</h2>
-          <img src="9.jpg"></img>
+          <Image src="9.jpg" alt="image" />
         </div>
       </div>
     </div>
